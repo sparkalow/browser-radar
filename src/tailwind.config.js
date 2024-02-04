3a6511a0709bb238a920621c10477e41c941c6e6
Brian Matthews <brian@weareindustrial.com>
2024-02-04 13:34:28 -0600

    Add color design tokens

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   mode: "jit",
   content: ['index.html', 'js/app.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(34 197 94)",
+        secondary: "#62adf6"
+      }
+    },
   },
   variants: {
     extend: {},
